53af5392a82e3a03e6d43858ccdfbefb447ddc92
Tyson <tysonC555@gmail.com>
2015-09-29 16:20:48 -0500

    can read frequency from generator

--- arduinoFreq/arduinoFreq.ino
+++ arduinoFreq/arduinoFreq.ino
@@ -1,14 +1,21 @@
-int tone1 = 0;
+volatile int tone1 = 0;
 int freq = 0;
-float prevInterruptTime;
+volatile long frequency =0;
+long prevInterruptTime=0;
+long divider = 1000000;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(7,INPUT);
   pciSetup(7);
+  Serial.println("In setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(2000);
+  Serial.println(tone1);
 
 }
 
@@ -23,11 +30,15 @@ void pciSetup(byte pin)
 
 // Pin change interrupt
 ISR (PCINT2_vect) // handle pin change interrupt for D0 to D7 here
-{
-        if ((millis() - prevInterruptTime) > freq) {
+{      //Serial.println("In interrupt");
+       frequency = divider/(micros() - prevInterruptTime);
+
+       if (frequency > 3000) {
            tone1 = 1;
         }
         else{
           tone1 = 0;
         }
+       prevInterruptTime=micros();
+       //Serial.println(frequency);
 }
